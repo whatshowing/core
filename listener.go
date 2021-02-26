0d56d4992b302e56f6c85b7a39ddae47ac8b1d92
brighton <bchabula@gmail.com>
2021-02-26 15:47:59 +0200

    updated listener and publisher

--- listener.go
+++ listener.go
@@ -6,35 +6,36 @@ import (
 	"log"
 )
 
-type Subscription interface {
-	parseMsg(msg *stan.Msg, pb *proto.Message) error
-	Listen() (stan.Subscription, error)
+type Listener interface {
+	parseMsg(msg *stan.Msg, pb proto.Message) error
+	Listen(onMsg func(pb proto.Message)) (stan.Subscription, error)
 }
 
-type subscription struct {
+type listener struct {
 	Subject        *Subject
 	QueueGroupName *QueueGroup
 	OnMessage      func()
 	Client         stan.Conn
-	ProtoMsg       *proto.Message
+	ProtoMsg       proto.Message
 	//AckWait int64
-	//subOption stan.SubscriptionOption
+	//subOption stan.ListenerOption
 }
 
-func (s *subscription) Listen() (stan.Subscription, error) {
+func (s *listener) Listen(onMsg func(pb proto.Message)) (stan.Subscription, error) {
 	return s.Client.QueueSubscribe(s.Subject.Name, s.QueueGroupName.Name, func(msg *stan.Msg) {
 		if er := s.parseMsg(msg, s.ProtoMsg); er != nil {
 			log.Printf("Error listening to subject %v. \n Error: %v \n", s.Subject.Name, er)
 		}
 		log.Printf("Listening event on subject %v\n", s.Subject.Name)
-
+		onMsg(s.ProtoMsg)
+		log.Println("event message processed successfully")
 	}, stan.DurableName(s.QueueGroupName.Name), stan.DeliverAllAvailable())
 }
 
-func (s *subscription) parseMsg(msg *stan.Msg, pb *proto.Message) error {
-	return proto.Unmarshal(msg.Data, *pb)
+func (s *listener) parseMsg(msg *stan.Msg, pb proto.Message) error {
+	return proto.Unmarshal(msg.Data, pb)
 }
 
-func NewSubscription(subject *Subject, group *QueueGroup, client stan.Conn, msg *proto.Message) Subscription {
-	return &subscription{Subject: subject, QueueGroupName: group, Client: client, ProtoMsg: msg}
+func NewListener(subject *Subject, group *QueueGroup, client stan.Conn, msg proto.Message) Listener {
+	return &listener{Subject: subject, QueueGroupName: group, Client: client, ProtoMsg: msg}
 }
--- publisher.go
+++ publisher.go
@@ -7,21 +7,20 @@ import (
 )
 
 type Publisher interface {
-	parseMsg(msg *proto.Message) ([]byte, error)
+	parseMsg(msg proto.Message) ([]byte, error)
 	Publish() error
 }
 
 type publisher struct {
 	Subject  *Subject
 	Client   stan.Conn
-	ProtoMsg *proto.Message
+	ProtoMsg proto.Message
 	//AckWait int64
-	//subOption stan.SubscriptionOption
 }
 
-func (p publisher) parseMsg(msg *proto.Message) ([]byte, error) {
+func (p publisher) parseMsg(msg proto.Message) ([]byte, error) {
 
-	return proto.Marshal(*msg)
+	return proto.Marshal(msg)
 }
 
 func (p publisher) Publish() error {
@@ -36,6 +35,6 @@ func (p publisher) Publish() error {
 	return p.Client.Publish(p.Subject.Name, d)
 }
 
-func NewPublisher(subject *Subject, client stan.Conn, msg *proto.Message) Publisher {
+func NewPublisher(subject *Subject, client stan.Conn, msg proto.Message) Publisher {
 	return &publisher{Subject: subject, Client: client, ProtoMsg: msg}
 }
