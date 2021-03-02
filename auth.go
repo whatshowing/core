6cdde7fefa3879e2004d7c0f780b3210336f1ae3
brighton <bchabula@gmail.com>
2021-03-02 11:48:06 +0200

    update auth service

--- auth.go
+++ auth.go
@@ -1,8 +1,23 @@
 package core
 
-const (
-	UserStatusRegistration = "registration"
-	UserStatusBlocked      = "blocked"
-	UserStatusDisabled     = "disabled"
-	UserStatusEnabled      = "enabled"
-)
+var UserStatuses = newSubjectRegistry()
+
+type UserStatus struct {
+	Name string
+}
+
+type userStatusRegistry struct {
+	Registration *UserStatus
+	Blocked      *UserStatus
+	Disabled     *UserStatus
+	Enabled      *UserStatus
+}
+
+func newUserStatusRegistry() *userStatusRegistry {
+	return &userStatusRegistry{
+		Registration: &UserStatus{Name: "registration"},
+		Blocked:      &UserStatus{Name: "blocked"},
+		Disabled:     &UserStatus{Name: "disabled"},
+		Enabled:      &UserStatus{Name: "enabled"},
+	}
+}
--- microservices.go
+++ microservices.go
@@ -1,7 +1,21 @@
 package core
 
-const (
-	AuthServiceName      = "auth"
-	MessagingServiceName = "messaging"
-	UserServiceName      = "user"
-)
+var MicroServices = newMicroServiceRegistry()
+
+type MicroService struct {
+	Name string
+}
+
+type serviceRegistry struct {
+	Auth      *MicroService
+	Messaging *MicroService
+	User      *MicroService
+}
+
+func newMicroServiceRegistry() *serviceRegistry {
+	return &serviceRegistry{
+		Auth:      &MicroService{Name: "auth"},
+		Messaging: &MicroService{Name: "messaging"},
+		User:      &MicroService{Name: "user"},
+	}
+}
