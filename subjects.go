0f73445d7b89921f3c279fa94837c937ee55a08e
brighton <bchabula@gmail.com>
2021-03-11 14:02:57 +0200

    Added more event subjects

--- subjects.go
+++ subjects.go
@@ -7,13 +7,35 @@ type Subject struct {
 }
 
 type subjectRegistry struct {
-	AuthChange  *Subject
-	UserCreated *Subject
+	AuthChange            *Subject
+	UserCreated           *Subject
+	UserUpdated           *Subject
+	UserArtistCreated     *Subject
+	UserArtistUpdated     *Subject
+	UserLabelCreated      *Subject
+	UserLabelUpdated      *Subject
+	UserPortfolioCreated  *Subject
+	UserPortfolioUpdated  *Subject
+	UserProductionCreated *Subject
+	UserProductionUpdated *Subject
+	UserCinemaCreated     *Subject
+	UserCinemaUpdated     *Subject
 }
 
 func newSubjectRegistry() *subjectRegistry {
 	return &subjectRegistry{
-		AuthChange:  &Subject{Name: "auth:change"},
-		UserCreated: &Subject{Name: "user:created"},
+		AuthChange:            &Subject{Name: "auth:change"},
+		UserCreated:           &Subject{Name: "user:user:created"},
+		UserUpdated:           &Subject{Name: "user:user:updated"},
+		UserArtistCreated:     &Subject{Name: "user:artist:created"},
+		UserArtistUpdated:     &Subject{Name: "user:artist:updated"},
+		UserLabelCreated:      &Subject{Name: "user:label:created"},
+		UserLabelUpdated:      &Subject{Name: "user:label:updated"},
+		UserPortfolioCreated:  &Subject{Name: "user:portfolio:created"},
+		UserPortfolioUpdated:  &Subject{Name: "user:portfolio:updated"},
+		UserProductionCreated: &Subject{Name: "user:production:created"},
+		UserProductionUpdated: &Subject{Name: "user:production:updated"},
+		UserCinemaCreated:     &Subject{Name: "user:cinema:created"},
+		UserCinemaUpdated:     &Subject{Name: "user:cinema:updated"},
 	}
 }
